56960df4317e90cde3925c36be28bb0b893ee118
daniel <daniel.lee@dradvice.com.tw>
2023-11-02 13:04:34 +0800

    fix: 如果沒有圖片的處理方式改變 問題： 進入細節頁面fragment，如果imageList為空直，沒有顯示圖片
    原因：
    優化程式碼
    
    調整項目：
    如果獲取到null or empty強制給他塞預設圖片

--- app/src/main/java/com/daniel/cathybktour/view/adapter/ImagePagerAdapter.kt
+++ app/src/main/java/com/daniel/cathybktour/view/adapter/ImagePagerAdapter.kt
@@ -1,6 +1,5 @@
 package com.daniel.cathybktour.view.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,21 +8,31 @@ import com.daniel.cathybktour.api.Image
 import com.daniel.cathybktour.databinding.ImageItemBinding
 import com.squareup.picasso.Picasso
 
-class ImagePagerAdapter(private val imageList: MutableList<Image?>? = mutableListOf(Image(".jpg", "drawable", ""))) :
-    RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder>() {
+class ImagePagerAdapter(imageListInput: MutableList<Image?>? = null) : RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder>() {
+
+    private val imageList: MutableList<Image?> = imageListInput ?: mutableListOf()
+
+    init {
+
+        if (imageList.isEmpty()) {
+
+            imageList.add(Image(".jpg", "drawable", ""))
+
+        }
+
+    }
 
     class ImageViewHolder(val binding: ImageItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(image: String?) {
 
-            if (!image.isNullOrEmpty()) {
+            if (image.isNullOrEmpty()) {
 
-                Picasso.get().load(image ?: "").placeholder(R.drawable.taipei_icon).into(binding.imageView)
+                Picasso.get().load(R.drawable.taipei_icon).into(binding.imageView)
 
             } else {
 
-                Log.d("TAG","checkpoint sssssss")
-                Picasso.get().load(R.drawable.taipei_icon).into(binding.imageView)
+                Picasso.get().load(image).placeholder(R.drawable.taipei_icon).into(binding.imageView)
 
             }
 
@@ -32,27 +41,25 @@ class ImagePagerAdapter(private val imageList: MutableList<Image?>? = mutableLis
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+
         val binding = ImageItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ImageViewHolder(binding)
-    }
-
-    override fun getItemCount(): Int {
-
-        return imageList?.size ?: 0
 
     }
+
+    override fun getItemCount(): Int = imageList.size
+
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
 
-        val imageUrl = imageList?.get(position)?.src
-
+        val imageUrl = imageList[position]?.src
         if (imageUrl == "drawable") {
 
             holder.bind(null)
 
         } else {
 
-            imageUrl?.let { holder.bind(it) }
+            holder.bind(imageUrl)
 
         }
 
